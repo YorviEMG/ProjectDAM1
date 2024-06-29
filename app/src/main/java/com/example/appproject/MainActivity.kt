e116368dc2e46234d4dc8f7283d3556bd61cd173
YorviEMG <yorviemg210@gmail.com>
2024-06-29 16:54:19 -0500

    3er commit v2

--- app/src/main/java/com/example/appproject/MainActivity.kt
+++ app/src/main/java/com/example/appproject/MainActivity.kt
@@ -8,10 +8,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.facebook.CallbackManager
+import com.facebook.login.LoginManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 private lateinit var btnPerfil: ImageButton
 private lateinit var btnLogout: ImageButton
+private lateinit var auth: FirebaseAuth
+private lateinit var authGoogle: GoogleSignIn
+private val callbackManager = CallbackManager.Factory.create()
 var usuName:String = ""
+var correo:String = ""
+var log:String = ""
+var rol:String = ""
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,29 +35,39 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        auth = Firebase.auth
         btnPerfil = findViewById(R.id.btnPerfilMain)
         btnLogout = findViewById(R.id.btnLogoutMain)
         btnPerfil.setOnClickListener{ goPerfil() }
-        btnLogout.setOnClickListener{ logout() }
+        btnLogout.setOnClickListener{
+            auth.signOut()
+            objGoogleSignIn.clientGoogle.signOut()
+            LoginManager.getInstance().logOut()
+            var intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
         initial()
     }
     fun initial(){
         var info = intent.extras
         if (info != null && info.containsKey("name")) {
             usuName = info.getString("name").toString()
+            correo = info.getString("correo").toString()
+            log = info.getString("log").toString()
+            rol = info.getString("rol").toString()
         }
     }
     fun goPerfil(){
         if (usuName != ""){
             var intent = Intent(this, PerfilActivity::class.java)
             intent.putExtra("name", usuName)
+            intent.putExtra("correo", correo)
+            intent.putExtra("log", log)
+            intent.putExtra("rol", rol)
             startActivity(intent)
         }else{
             Toast.makeText(this, "No existe intent",Toast.LENGTH_SHORT)
         }
-    }
-    fun logout(){
-        var intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/appproject/entidad/Usuario.kt
+++ app/src/main/java/com/example/appproject/entidad/Usuario.kt
@@ -1,4 +1,4 @@
 package com.example.appproject.entidad
 
-class Usuario(var usuario:String, var correo:String, var nombre:String, var rol:String) {
+class Usuario(var cod:Int, var nombre:String, var correo:String, var clave:String, var log:String, var rol:String) {
 }
